8467f25166069cc2c685d7659700cbe02628bf9e
Beksultan <beksultan.sagnaev@gmail.com>
2023-02-09 11:20:03 +0600

    var init checking answer

--- main.js
+++ main.js
@@ -304,6 +304,8 @@ const birdsData = [
 let playSVG = '<svg viewBox="-200 0 1200 1000"><path fill="#00bc8c" d="M96.51 11.97c-31.23 8.05-53.26 32.76-63.42 71.27-3.45 12.84-3.64 29.7-3.64 416.71s.19 403.87 3.64 416.71c16.09 60.74 61.69 86.03 120.9 67.25 9-2.87 53.65-25.1 116.49-58.24 56.14-29.51 221.29-116.3 367.28-192.93 145.99-76.64 271.29-143.31 278.38-148.1 39.28-25.68 59.59-63.04 53.26-97.52-4.79-26.63-24.33-53.07-52.88-71.65C892 399.37 172.58 22.32 154.95 16.38c-18.97-6.33-43.3-8.24-58.44-4.41z"></path></svg>'
 let pauseSVG = '<svg viewBox="0 0 47.607 47.607"><path fill="#00bc8c" d="M17.991 40.976a6.631 6.631 0 01-13.262 0V6.631a6.631 6.631 0 0113.262 0v34.345zM42.877 40.976a6.631 6.631 0 01-13.262 0V6.631a6.631 6.631 0 0113.262 0v34.345z"></path></svg>'
 
+let randomNumber
+
 let randomBird = () => {
   randomNumber = Math.floor(Math.random() * birdsData[0].length)
 
@@ -315,12 +317,12 @@ let randomBird = () => {
   audioPlayer.appendChild(audio);
 
   audio.setAttribute('src', birdsData[0][randomNumber].audio);
-  listItems = document.querySelectorAll('li')
-  console.log(listItems)
 
+  var listItems = document.querySelectorAll('li')
   birdsVar = birdsData[0].map(bird => bird.name)
-  // listItems.forEach(li => li.innerHTML = birdsVar.forEach(bird => bird))
-  console.log(birdsVar.forEach(bird => bird))
+  for(let i = 0; i<birdsVar.length; i++) {
+    listItems[i].innerHTML = birdsVar[i]
+  }
 }
 
 randomBird()
@@ -342,8 +344,22 @@ playBtn.addEventListener('click', function() {
     flag = 0
   }
 
+  let circle = document.querySelector('.circle')
+  let timeBar = document.querySelector('.time_bar').offsetWidth
   let duration = document.querySelector('.duration')
-  console.log(audio.duration)
+  let durTime = Math.floor(audio.duration)
+  
+//   function progress(timeleft, timetotal, $element) {
+//     var progressBarWidth = timeleft * $element.width() / timetotal;
+//     $element.find('div').animate({ width: progressBarWidth }, 500).html(Math.floor(timeleft/60) + ":"+ timeleft%60);
+//     if(timeleft > 0) {
+//         setTimeout(function() {
+//             progress(timeleft - 1, timetotal, $element);
+//         }, 1000);
+//     }
+// };
+
+// progress(60, 60, $('#progressBar'));
 
   let res = ''
 
@@ -355,4 +371,22 @@ playBtn.addEventListener('click', function() {
   res += Math.floor(audio.duration % 60)
 
   duration.innerHTML = res
+})
+
+let listItems = document.querySelectorAll('li')
+
+listItems.forEach(li => {
+  li.addEventListener('click', function() {
+    if(this.innerHTML === birdsData[0][randomNumber].name) {
+      let audio = document.createElement('audio')
+      audio.setAttribute('src', './karlson_na_sms.mp3')
+      audio.play()
+      getComputedStyle(this, ':before').style.color = "red"
+    }
+    else {
+      let audio = document.createElement('audio')
+      audio.setAttribute('src', './ne_prav.mp3')
+      audio.play()
+    }
+  })
 })
